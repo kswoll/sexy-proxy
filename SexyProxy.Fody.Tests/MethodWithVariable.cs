42fd57887f6bf772e9f5f8fd8f5eb43b640318b7
Kirk Woll <kirk.woll@gmail.com>
2017-04-25 11:26:16 -0700

    Clear out scope info when replacing MethodBody

---
+++ SexyProxy.Fody.Tests/MethodWithVariable.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace SexyProxy.Fody.Tests
+{
+	[TestFixture]
+	public class MethodWithVariable
+	{
+		[Test]
+		public void Compiles()
+		{
+		}
+
+		public class MethodWithVariableClass : IProxy
+		{
+			public InvocationHandler InvocationHandler { get; }
+
+			public void MethodWithVariableMethod()
+			{
+				int i = 0;
+			}
+		}
+	}
+}
